08e76d73f4112ffeaeb8b3130ca4665e3eb2d340
Vinicius Celms <by.vinicius@gmail.com>
2015-10-25 14:06:16 -0200

    Criação da classe SetorCompras implementando a interface Funcionario.

---
+++ pim4sem.aev.business/src/pim4sem/aev/business/funcionarios/SetorCompras.java
@@ -0,0 +1,30 @@
+package pim4sem.aev.business.funcionarios;
+
+public class SetorCompras implements Funcionario {
+
+	public SetorCompras(){
+		
+	}
+	
+	public void CadastraFuncionario(String recebeNome, String recebeCargo, double recebeSalario,
+			String recebeGerente, boolean recebeCargoGerencial){
+		
+	}
+	
+	public void AlterarCargoFuncionario(String recebeNome, int recebeMatricula){
+		
+	}
+	
+	public void DemitirFuncionario(String recebeNome, int recebeMatricula){
+		
+	}
+	
+	public void PesquisaFuncionario(String recebeNome){
+
+	}
+	
+	public void PesquisaFuncionario(int recebeMatricula){
+
+	}
+	
+}
