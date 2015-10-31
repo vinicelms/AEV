60cf5657640a781b6a5b24b294c1c08c381b292f
Vinicius Celms <by.vinicius@gmail.com>
2015-10-31 13:29:13 -0200

    Implementação dos atributos da classe Usuario.

--- pim4sem.aev.business/src/pim4sem/aev/business/funcionarios/Usuario.java
+++ pim4sem.aev.business/src/pim4sem/aev/business/funcionarios/Usuario.java
@@ -2,6 +2,12 @@ package pim4sem.aev.business.funcionarios;
 
 public class Usuario {
 	
+	private String login;
+	private String senha;
+	private String nome;
+	private boolean funcionario;
+	private String funcao;
+	
 	
 	
 }
