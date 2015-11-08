a11dba46bc8af2837a192d1f5d27c81994eec897
Vinicius Celms <by.vinicius@gmail.com>
2015-11-08 04:46:43 -0200

    Criacao do pacote 'cliente' e classe 'Cliente'

---
+++ pim4sem.aev.business/src/pim4sem/aev/business/cliente/Cliente.java
@@ -0,0 +1,35 @@
+package pim4sem.aev.business.cliente;
+
+public class Cliente {
+
+	private int codigo;
+	private String nome;
+	private long cpf;
+	private char sexo;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+}
