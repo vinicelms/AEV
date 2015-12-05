44b199f9b9aeef24e063ffc6e0c548e90729bdc9
ed-dalecio <edson.dalecio@gmail.com>
2015-12-05 00:11:56 -0200

    Tela de estoque (desktop) finalizada.

--- pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/EstoqueTelaController.java
+++ pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/EstoqueTelaController.java
@@ -34,9 +34,32 @@ public class EstoqueTelaController {
 	private Button btnLimpar;
 
 	@FXML
-	private void initialize(){
+	private void initialize() throws SQLException{
 		btnPesquisar.disableProperty().bind(txtId.textProperty().isEmpty());
 		btnLimpar.disableProperty().bind(txtId.textProperty().isEmpty());
+		
+		
+		Produto p1 = new Produto();
+		ProdutoDAO pdd = new ProdutoDAO();
+		
+		/*
+		p1.setCodigo(1122);
+		p1.setNome("crocs");
+		p1.setMarca("feio");
+		p1.setCor("verde");
+		p1.setValorCompra(10);
+		p1.setValorVenda(300);
+		p1.setDescricao("yhyhyhyedede");
+		p1.setTamanho(40);
+		p1.setTipoProduto("2");
+		p1.setQuantidadeEstoque(25);
+		p1.setQuantidadeMinima(15);
+		p1.setPosicaoEstoque('a');
+		p1.setPosicaoPrateleira(1);
+		pdd.registrarProduto(p1);
+		*/
+		
+		
 	}
 	
 	@FXML
@@ -56,17 +79,30 @@ public class EstoqueTelaController {
 	}
 	
 	@FXML
-	private void onPesquisar() throws SQLException{
-		int numProd = 0;
-		
-		ProdutoDAO pd = new ProdutoDAO();
+	private void onPesquisar() throws SQLException, IndexOutOfBoundsException{
+		ProdutoDAO pd = new ProdutoDAO();	
 		List<Produto> produtos = new ArrayList<Produto>();
-		produtos = pd.retornaProduto("id_produto", txtId.getText());
-		
-		for(Produto prod : produtos){
-			numProd++;
+		produtos = pd.retornaProduto("Codigo", txtId.getText());
+			
+		try {
+			txtNome.setText(produtos.get(0).getNome());
+			txtDesc.setText(produtos.get(0).getDescricao());
+			txtCor.setText(produtos.get(0).getCor());
+			txtMarca.setText(produtos.get(0).getMarca());
+			txtQuantidade.setText(String.valueOf(produtos.get(0).getQuantidadeEstoque()));
+			txtValor.setText(String.valueOf(produtos.get(0).getValorVenda()));
+		} catch (IndexOutOfBoundsException e) {
+			onLimpar();
+			txtId.setText("ID INEXISTENTE");
 		}
-		txtQuantidade.setText(String.valueOf(numProd));
+
+		//TESTE
+		/*
+		produtos = pd.retornaProduto(null,null);
+		for(int i = 0; i < produtos.size(); i++){
+			System.out.println(produtos.get(i).getCodigo());
+		}*/
+				
 	}
 	
 }
