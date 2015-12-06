c170ca0991b958c5e5b6bed4f832654c5c9e6cab
ed-dalecio <edson.dalecio@gmail.com>
2015-12-05 22:32:29 -0200

    Tela de Menu concluida, correção de Estoque e framework para troca de tela

--- pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/EstoqueTelaController.java
+++ pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/EstoqueTelaController.java
@@ -11,38 +11,26 @@ import javafx.scene.control.TextField;
 import pim4sem.aev.business.DAO.ProdutoDAO;
 import pim4sem.aev.business.produto.Produto;
 
-public class EstoqueTelaController {
+public class EstoqueTelaController implements IControlledScreen{
 
 	
 	@FXML
-	private TextField txtId;
-	@FXML
-	private TextField txtNome;
-	@FXML
-	private TextField txtDesc;
-	@FXML
-	private TextField txtCor;
-	@FXML
-	private TextField txtMarca;
-	@FXML
-	private TextField txtQuantidade;
-	@FXML
-	private TextField txtValor;
+	private TextField txtId, txtNome, txtDesc, txtCor, txtMarca, txtQuantidade, txtValor;
 	@FXML 
-	private Button btnPesquisar;
-	@FXML 
-	private Button btnLimpar;
+	private Button btnPesquisar, btnLimpar;
+	
+	ScreensController myController;
 
 	@FXML
 	private void initialize() throws SQLException{
 		btnPesquisar.disableProperty().bind(txtId.textProperty().isEmpty());
 		btnLimpar.disableProperty().bind(txtId.textProperty().isEmpty());
+		txtId.setFocusTraversable(false);
 		
-		
+		/*
 		Produto p1 = new Produto();
 		ProdutoDAO pdd = new ProdutoDAO();
 		
-		/*
 		p1.setCodigo(1122);
 		p1.setNome("crocs");
 		p1.setMarca("feio");
@@ -62,11 +50,25 @@ public class EstoqueTelaController {
 		
 	}
 	
+	@Override
+	public void setScreenParent(ScreensController screenParent){
+		myController = screenParent;	
+	}	
+	
+	//Método para retornar ao menu principal
+	@FXML 
+	public void onMenu(){
+		myController.setScreen(Main.MenuTelaID);
+		onLimpar();
+	}
+	
+	//Método para encerrar o programa
 	@FXML
 	private void onExit(){
 		Platform.exit();
 	}
 	
+	//Esse método é responsável por limpar os campos
 	@FXML
 	private void onLimpar(){
 		txtId.setText(null);
@@ -78,19 +80,26 @@ public class EstoqueTelaController {
 		txtValor.setText(null);
 	}
 	
+	//Esse método é responsável pela pesquisa
 	@FXML
 	private void onPesquisar() throws SQLException, IndexOutOfBoundsException{
 		ProdutoDAO pd = new ProdutoDAO();	
 		List<Produto> produtos = new ArrayList<Produto>();
 		produtos = pd.retornaProduto("Codigo", txtId.getText());
-			
+
 		try {
-			txtNome.setText(produtos.get(0).getNome());
-			txtDesc.setText(produtos.get(0).getDescricao());
-			txtCor.setText(produtos.get(0).getCor());
-			txtMarca.setText(produtos.get(0).getMarca());
-			txtQuantidade.setText(String.valueOf(produtos.get(0).getQuantidadeEstoque()));
-			txtValor.setText(String.valueOf(produtos.get(0).getValorVenda()));
+			for(char letra : txtId.getText().toCharArray()){
+				if(letra >= '0' && letra <= '9'){
+					txtNome.setText(produtos.get(0).getNome());
+					txtDesc.setText(produtos.get(0).getDescricao());
+					txtCor.setText(produtos.get(0).getCor());
+					txtMarca.setText(produtos.get(0).getMarca());
+					txtQuantidade.setText(String.valueOf(produtos.get(0).getQuantidadeEstoque()));
+					txtValor.setText(String.valueOf(produtos.get(0).getValorVenda()));
+				} else {
+					throw new IndexOutOfBoundsException();
+				}				
+			} 
 		} catch (IndexOutOfBoundsException e) {
 			onLimpar();
 			txtId.setText("ID INEXISTENTE");
@@ -103,6 +112,5 @@ public class EstoqueTelaController {
 			System.out.println(produtos.get(i).getCodigo());
 		}*/
 				
-	}
-	
+	}	
 }
--- pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/Main.java
+++ pim4sem.aev.desktop/src/pim4sem/aev/desktop/controledetelas/Main.java
@@ -1,19 +1,44 @@
 package pim4sem.aev.desktop.controledetelas;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
+	
+	public static String MenuTelaID = "MenuTela";
+	public static String MenuTelaFile = "MenuTela.fxml";
+	public static String EstoqueTelaID = "EstoqueTela";
+	public static String EstoqueTelaFile = "EstoqueTela.fxml";
+	
+	@Override
+	public void start(Stage primaryStage){
+		ScreensController mainContainer = new ScreensController();
+		mainContainer.loadScreen(Main.MenuTelaID, MenuTelaFile);
+		mainContainer.loadScreen(Main.EstoqueTelaID, EstoqueTelaFile);
+		
+		mainContainer.setScreen(Main.MenuTelaID);
+		
+		Group root = new Group();
+		root.getChildren().addAll(mainContainer);
+		Scene scene = new Scene(root, 800, 550);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Loja de Calçados");
+		primaryStage.show();
+	}
+	
+	
+	
+	
+	
+/*
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
 			String sceneName;
-			//sceneName = "MenuTela.fxml";
-			sceneName = "EstoqueTela.fxml";
+			sceneName = "MenuTela.fxml";
+			//sceneName = "EstoqueTela.fxml";
 			
 			Pane root = FXMLLoader.load(getClass().getResource(sceneName));
 			Scene scene = new Scene(root,800,600);
@@ -24,7 +49,7 @@ public class Main extends Application{
 			e.printStackTrace();
 		}		
 	}
-
+*/
 	public static void main(String[] args) {
 		launch(args);
 	}
