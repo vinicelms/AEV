14fdb5d097f3a45b90a76b39de80a15822592362
Vinicius Celms <by.vinicius@gmail.com>
2016-03-30 17:00:09 -0300

    Alteração da mensagem de conexão ao banco

--- src/pim4sem/aev/business/DAO/TestandoConnectionFactory.java
+++ src/pim4sem/aev/business/DAO/TestandoConnectionFactory.java
@@ -10,7 +10,7 @@ public class TestandoConnectionFactory {
 		
 		Connection conn = new ConnectionFactory().getConnection();
 		
-		System.out.println("Conexão aberta, Javão!");
+		System.out.println("Connection opened!");
 		
 		conn.close();
 
